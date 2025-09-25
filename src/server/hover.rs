c80a0361b4b7c21de8bcaba3a65f13513d7fd73c
xijniN <xijnin@gmail.com>
2025-09-25 16:00:34 -0500

    Markdown hover

--- src/server/hover.rs
+++ src/server/hover.rs
@@ -3,57 +3,74 @@ use lsp_types::{Hover, HoverContents, HoverParams, MarkupContent, MarkupKind, Po
 use vfs::FileSystem;
 
 use crate::server::{
-        document::{Document, PRIMITIVE_TYPES}, mod_api::ModApi, utils::get_spot_info, Server
-    };
+    document::{Document, PRIMITIVE_TYPES}, mod_api::ModApi, utils::get_spot_info, Server
+};
+
+struct HoverContent {
+    code: String,
+    text: String,
+}
+
+impl HoverContent {
+    pub fn new_code_only(code: String) -> HoverContent {
+        HoverContent { code, text: "".to_string() }
+    }
+    pub fn new_text_only(text: String) -> HoverContent {
+        HoverContent { code: "".to_string(), text }
+    }
+}
 
 impl Server {
     fn get_hover(
         mod_api: &ModApi,
         document: &Document,
         node: &tree_sitter::Node<'_>,
-    ) -> Option<String> {
+    ) -> Option<HoverContent> {
         let range = node.byte_range();
         if node.kind() == "identifier" {
             let name = &document.content[range];
             let spot_info = get_spot_info(document, node);
             for var in spot_info.variables.into_iter() {
                 if var.name.as_bytes() == name {
-                    return Some(format!("{}: {}", var.name, var.r#type.as_str()));
+                    let code = format!("{}: {}", var.name, var.r#type.as_str());
+                    return Some(HoverContent::new_code_only(code));
                 }
             }
 
             let name = String::from_utf8(name.to_vec()).ok()?;
 
             if let Some(func) = mod_api.game_functions.get(&name) {
-                let mut text = func.format(&name);
-                text.push_str("\n\n");
+                let declaration = func.format(&name);
 
-                text.push_str(&func.description);
-
-                return Some(text);
+                return Some(HoverContent {
+                    code: declaration,
+                    text: func.description.to_string(),
+                });
             }
 
         } else if node.kind() == "helper_identifier" {
             let name = &document.content[range];
             
             if let Some(helper) = document.helpers.iter().find(|helper| helper.name.as_bytes() == name) {
-                return Some(helper.format());
+                return Some(HoverContent::new_code_only(helper.format()));
             }
         } else if node.kind() == "on_identifier" {
             let name = String::from_utf8(document.content[range].to_vec()).ok()?;
             if let Some(entity) = mod_api.entities.get(&document.entity_type) {
                 if let Some(on_func) = entity.on_functions.get(&name) {
-                    let mut text = name;
-                    text.push_str("\n\n");
-
-                    text.push_str(&on_func.description);
-                    return Some(text);
+                    return Some(HoverContent {
+                        code: name,
+                        text: on_func.description.to_string(),
+                    });
                 }
             }
         } else if node.kind() == "type" {
             let name = String::from_utf8(document.content[range].to_vec()).ok()?;
             if let Some(desc) = PRIMITIVE_TYPES.get(name.as_str()) {
-                return Some(format!("{}\n\n{}", name, desc));
+                return Some(HoverContent {
+                    code: name,
+                    text: desc.to_string(),
+                });
             }
         }
 
@@ -93,22 +110,24 @@ impl Server {
             connection.sender.send(Message::Response(Response::new_ok(id, serde_json::Value::Null))).unwrap();
             return;
         }
-        let mut content = content.unwrap().as_bytes().to_vec();
 
-        let var = document
-            .global_vars
-            .iter()
-            .find(|var| var.name.as_bytes() == &content);
-        if let Some(var) = var {
-            content = format!("{}: {}", var.name, var.r#type.as_str())
-                .as_bytes()
-                .to_vec();
+        let content = content.unwrap();
+        let mut hover_text = String::new();
+
+        if !content.code.is_empty() {
+            hover_text = format!("```rust\n{}\n```", content.code);
+        }
+        if !content.text.is_empty() {
+            if !content.code.is_empty() {
+                hover_text.push('\n');
+            }
+            hover_text.push_str(&content.text);
         }
 
         let res = Response::new_ok(id, Hover {
             contents: HoverContents::Markup(MarkupContent {
-                kind: MarkupKind::PlainText,
-                value: String::from_utf8(content).unwrap(),
+                kind: MarkupKind::Markdown,
+                value: String::from_utf8(hover_text.as_bytes().to_vec()).unwrap(),
             }),
             range: Some(Range {
                 start: Position {
